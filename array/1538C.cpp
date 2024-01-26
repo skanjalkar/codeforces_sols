842b2cb9b5dffb98b7cc92c0355372e16dc2b620
skanjalkar <shreyaskanjalkar@gmail.com>
2024-01-25 20:41:45 -0500

    daily questions

--- array/1538C.cpp
+++ array/1538C.cpp
@@ -3,25 +3,31 @@
 using namespace std;
 
 void solve() {
-    int n, l, r;
+    long long n, l, r;
     cin >> n >> l >> r;
     int a[n];
     for (int i = 0; i < n; ++i) {
         cin >> a[i];
     }
     sort(a, a+n);
-    function<int(int)>lb = [&](int v) {
-        return -1;
+    auto lb = [&](long long v){
+        return (lower_bound(a, a + n, v) - a);
     };
-    function<int(int)>ub = [&](int v) {
-        return -1;
+    auto ub = [&](long long v) {
+        return (upper_bound(a, a + n, v) - a);    
     };
-    int ans = 0;
+    long long ans = 0;
     for (int i = 0; i < n; ++i) {
-        int hidx = lb(r - a[i]);
-        int lidx = ub(l - a[i]);
+        int hidx = ub(r - a[i]);
+        int lidx = lb(l - a[i]);
+        ans -= lidx;
+        ans += hidx;
+        if (l <= 2 * a[i] && 2 * a[i] <= r) {
+            --ans;
+        }
         //hidx = min(hidx, 
     }
+    cout << ans / 2 << endl;
     return;
 }
 
