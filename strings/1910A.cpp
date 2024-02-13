0d8a9e87f343599d71cad20c768b119713d9661f
skanjalkar <shreyaskanjalkar@gmail.com>
2024-02-12 19:36:59 -0500

    1910 A is kotlin only gg

---
+++ strings/1910A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    string s;
+    cin >> s;
+    
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
