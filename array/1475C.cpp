a72378d6d36b9048b5917849ea2ee92a7c4bef3e
skanjalkar <shreyaskanjalkar@gmail.com>
2024-02-14 21:35:20 -0500

    not done 1475

---
+++ array/1475C.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    int a, b, k;
+    cin >> a >> b >> k;
+    vector<vector<int>> pairs(k, vector<int>(2, 0));
+    for (int i = 0; i < k; ++i) {
+        cin >> pairs[i][0];
+    }
+    for (int i = 0; i < k; ++i) {
+        cin >> pairs[i][1];
+    }
+
+    long long ans = 0;
+ 
+    
+    
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
