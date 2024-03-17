1649ae8e5d7fc9e3f0591e68fe1d7638a1fb0936
skanjalkar <shreyaskanjalkar@gmail.com>
2024-03-17 19:33:28 -0400

    bad contest, couldnt do b

--- contests/edu/163edud2/b.cpp
+++ contests/edu/163edud2/b.cpp
@@ -2,9 +2,34 @@
 
 using namespace std;
 
-void solve() {
+pair<int, int> ok(int num) {
+    string tmp = to_string(num);
+    for (int i = 0; i < tmp.size() - 1; ++i) {
+        if (tmp[i] > tmp[i+1]) return {-1,-1};
+    }
+    const int m = tmp.size();
+    return {tmp[0]-'0', tmp[m-1]-'0'};
+}
 
-    return;
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> ans(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> ans[i];
+    }
+    int mx = ans[n-1];
+    for (int i = n-2; i >= 0; --i) {
+        if (ans[i] > mx) {
+            pair<int, int> tmp = ok(ans[i]);
+            if (tmp.first == -1 || tmp.second > mx) {
+                cout << "NO" << "\n";
+                return;
+            }
+            mx = tmp.first;
+        }
+    }
+    cout << "YES" << "\n";
 }
 
 int main() {
